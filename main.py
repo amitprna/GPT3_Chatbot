09bb9e9deaebbe92631f1a5d05477a38d3559ecb
Amit Kumar <70321164+amitprna@users.noreply.github.com>
2021-12-23 20:49:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def load_lottieurl(url: str):
     return None
   return r.json()
 
-url = "https://assets7.lottiefiles.com/packages/lf20_0nnf0are/data.json"
+url = "https://assets6.lottiefiles.com/private_files/lf30_bb9bkg1h.json"
 res_json = load_lottieurl(url)
 st_lottie(res_json)
 
